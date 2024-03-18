3fdf3c00a4eed5e2f0dde83703830a31b7163832
villsun <lonukkola@gmail.com>
2024-03-18 21:42:01 +0100

    Add fucntion Reading_from_File

--- Lesson_16(homework)/Homework.cpp
+++ Lesson_16(homework)/Homework.cpp
@@ -11,6 +11,42 @@ struct Person
 	char phone_number[200];
 };
 
+Person* Reading_from_File(int& size)
+{
+	FILE* p_file;
+	char str_file_name[200];
+	cout << "Enter file\n";
+	cin.getline(str_file_name, 200);
+
+	p_file = fopen(str_file_name, "r");
+
+	if (p_file == nullptr)
+	{
+		cout << "Error!\n";
+		return nullptr;
+	}
+
+	char str_temp[200];
+	int counter = 0;
+
+	while (fgets(str_temp, 200, p_file) != nullptr)
+	{
+		counter++;
+	}
+
+	fseek(p_file, 0, SEEK_SET);
+	Person* people = new Person[counter];
+
+	for (int i = 0; i < counter; i++)
+	{
+		fscanf(p_file, "%s %i %s", people[i].name, &people[i].age, people[i].phone_number);
+	}
+
+	size = counter;
+
+	return people;
+}
+
 int main()
 {
 

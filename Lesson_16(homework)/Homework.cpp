74bd99a8f8433f99ad9347c6dd1fac28a04c8371
villsun <lonukkola@gmail.com>
2024-03-18 21:43:16 +0100

    Add function Edit_Person

--- Lesson_16(homework)/Homework.cpp
+++ Lesson_16(homework)/Homework.cpp
@@ -47,6 +47,56 @@ Person* Reading_from_File(int& size)
 	return people;
 }
 
+void Edit_Person(Person* people, int size)
+{
+	int user_input;
+	cout << "Enter the employee(1 - " << size << ")\n";
+
+	do
+	{
+		cin >> user_input;
+		if (user_input<1 || user_input>size)
+		{
+			cout << "ERROR!\n";
+		}
+	} while (user_input<1 || user_input>size);
+
+	int user_input_edit;
+
+	cout << "What do you want edit:\n1 - Lasr name\n2 - Age\n3 - Phone number\n";
+	cin >> user_input;
+
+	char str_user_input[200];
+	int age;
+
+	switch (user_input)
+	{
+	case 1:
+		cin.ignore();
+		cout << "Enter last name\n";
+		cin.getline(str_user_input, 200);
+
+		strcpy(people[user_input - 1].name, str_user_input);
+		break;
+	case 2:
+		/*cin.ignore();*/
+		cout << "Enter age\n";
+		cin >> age;
+
+		people[user_input - 1].age = age;
+		break;
+	case 3:
+		cin.ignore();
+		cout << "Enter phone number\n";
+		cin.getline(str_user_input, 200);
+
+		strcpy(people[user_input - 1].phone_number, str_user_input);
+		break;
+	default:
+		break;
+	}
+}
+
 int main()
 {
 
